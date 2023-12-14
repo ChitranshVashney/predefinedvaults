1255ad3bb2738803ae5479ae96a54a12cacf8652
Chitranshu <varshneychinu1204@gmail.com>
2023-12-14 20:33:00 +0530

    m

--- ethvault.js
+++ ethvault.js
@@ -1,17 +1,18 @@
 const fs = require("fs");
 const Moralis = require("moralis").default;
-const { eth } = require("./newvault");
+const { bnb } = require("./newvault");
 const { error } = require("console");
 async function vaultdetails() {
   let data = [];
   await Moralis.start({
     apiKey:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImExYjhmNjE0LTczNmMtNGY4NS1hODE4LTBkYTg1NmMwZjYyOCIsIm9yZ0lkIjoiMjkzNjc5IiwidXNlcklkIjoiMzAwNTE5IiwidHlwZUlkIjoiOGQ1MzRiOTUtNDJkZS00MjAxLThkNjUtNzc5ZDM3OTE0NGIzIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2ODYyMjIzOTcsImV4cCI6NDg0MTk4MjM5N30.eqF4vyLycWpYR7jV8KB8vbwThXFiNGTCgf3oB3-If4E",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImE3ZmNmMzdiLTg1NWQtNDAyOC1iZWJlLTc1OWZlMDlkYjgwZCIsIm9yZ0lkIjoiMzY4MTk5IiwidXNlcklkIjoiMzc4NDE4IiwidHlwZUlkIjoiMDczZTVhM2EtOTNkZS00Yjg5LWE0ODItZTFiMWYxMjUxMzAzIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDI1NjM1NDgsImV4cCI6NDg1ODMyMzU0OH0.S1eIJPCOFa6xitDuSQYrw-fCyAxkaKTORkL9EzQvNDc",
   });
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   async function details(address) {
     try {
       const response = await Moralis.EvmApi.token.getTokenPrice({
-        chain: "0x1",
+        chain: "0x38",
         include: "percent_change",
         address: address,
       });
@@ -20,9 +21,9 @@ async function vaultdetails() {
     } catch (e) {
     }
   }
-  for (i in eth) {
+  for (i in bnb) {
     try {
-      let a = await details(eth[i].address.toLowerCase());
+      let a = await details(bnb[i].address.toLowerCase());
       let obj = {};
       let coinObj = [];
       let obj1 = {};
@@ -33,14 +34,14 @@ async function vaultdetails() {
       obj1["coinSymbol"] = a.tokenSymbol;
       obj1["coinAddress"] = a.tokenAddress;
       coinObj.push(obj1);
-      obj["vaultId"] = 200 + i;
+      obj["vaultId"] = 400 + i;
       obj["vaultName"] = a.tokenName;
       obj["vaultAddress"] = a.tokenAddress;
       obj["vaultSymbol"] = a.tokenSymbol;
       obj["vaultPrice"] = a.usdPrice;
       obj[
         "vaultDescription"
-      ] = `Smart contract address of this token is ${a.tokenAddress}`;
+      ] = `Smart contract address of this index is ${a.tokenAddress}`;
       obj["vaultBuytype"] = "Market Driven";
       obj["vaultStrategy"] = "Aggressive";
       obj["vaultRebalancing"] = true;
@@ -48,13 +49,14 @@ async function vaultdetails() {
       obj["vaultProvider"] = "LiFi";
       obj["vaultCoins"] = coinObj;
       obj["vaultStatus"] = true;
-      obj["chainID"] = "1";
+      obj["chainID"] = "56";
       obj["tradeable"] = 1;
       obj["createdBy"] = null;
       obj["modifiedBy"] = null;
       obj["createdAt"] = "2023-12-14T11:26:56.000Z";
       obj["updatedAt"] = "2023-12-14T11:26:56.000Z";
       data.push(obj);
+      await delay(1500);
     } catch (error) {
       continue;
     }
